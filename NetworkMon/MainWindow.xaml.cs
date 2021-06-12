73568e2fe7b6eb83b7930fc5ef5283dccf299902
karthikeyen <karthikeyen.ktm@gmail.com>
2021-06-12 23:26:15 +0530

    calling core logic .

--- NetworkMon/MainWindow.xaml.cs
+++ NetworkMon/MainWindow.xaml.cs
@@ -1,11 +1,11 @@
-﻿using NetworkMon.Helpers;
+﻿using Core;
+using NetworkMon.Helpers;
 using NetworkMon.UI;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Net.NetworkInformation;
-using System.Timers;
+using System.Threading;
 using System.Windows;
 
 namespace NetworkMon
@@ -13,12 +13,11 @@ namespace NetworkMon
     public partial class MainWindow : Window
     {
         public bool IsRunning = true;
-        private Timer _timer = new Timer();
         private EventLog eventLog = new EventLog();
 
-        private int count = 3;
-        private List<double> buffer = new List<double>();
-        private double _speed;
+        private NetworkInterface myNetworkAdapter;
+        private NetworkSpeed myDownloadSpeed;
+        private NetworkSpeed myUploadSpeed;
 
         public MainWindow()
         {
@@ -30,9 +29,6 @@ namespace NetworkMon
             this.Left = SystemParameters.PrimaryScreenWidth - 250;
             this.Top = SystemParameters.PrimaryScreenHeight - 100;
 
-            this._timer.Elapsed += _timer_Elapsed;
-            this._timer.Start();
-
             if (!EventLog.SourceExists("NetMonSource"))
             {
                 EventLog.CreateEventSource("NetMonSource", "Application");
@@ -40,12 +36,15 @@ namespace NetworkMon
 
             eventLog.Source = "NetMonSource";
 
+            myDownloadSpeed = new NetworkSpeed();
+            myUploadSpeed = new NetworkSpeed();
+
             TrayIconManager.SetupTrayIcon();
         }
 
         private void MainWindow_Closed(object sender, EventArgs e)
         {
-            this._timer.Stop();
+            this.IsRunning = false;
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
@@ -53,6 +52,19 @@ namespace NetworkMon
             eventLog.WriteEntry("Process started", EventLogEntryType.Information);
 
             StartupHelper.SetRunAtStartupEnabled(true);
+
+            new Thread(startTick).Start();
+        }
+
+        void startTick()
+        {
+            while (this.IsRunning)
+            {
+                if (NetworkInterface.GetIsNetworkAvailable())
+                {
+                    getConnectionInfo();
+                }
+            }
         }
 
         NetworkInterface GetConnectedNetworkInterface()
@@ -78,78 +90,31 @@ namespace NetworkMon
             }
         }
 
-        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            if (this.IsRunning && NetworkInterface.GetIsNetworkAvailable())
-            {
-                getConnectionInfo();
-            }
-            else
-            {
-                this._timer.Stop();
-            }
-        }
-
         private void getConnectionInfo()
         {
             try
             {
-                NetworkInterface adapter = GetConnectedNetworkInterface();
-                if (adapter == null) return;
-
-                long received = adapter.GetIPv4Statistics().BytesReceived;
-                string log = string.Empty;
-
-                double speed = Speed(received);
-                prevValue = received;
-
-                if (buffer.Count < count)
+                if (myNetworkAdapter == null)
                 {
-                    buffer.Add(speed);
+                    myNetworkAdapter = GetConnectedNetworkInterface();
                 }
 
-                if (buffer.Count == count)
+                long received = myNetworkAdapter.GetIPv4Statistics().BytesReceived;
+                long sent = myNetworkAdapter.GetIPv4Statistics().BytesSent;
+
+                var receivedSpeed = myDownloadSpeed.GetSpeed(received);
+                var sentSpeed = myUploadSpeed.GetSpeed(sent);
+
+                this.Dispatcher.Invoke(() =>
                 {
-                    _speed = buffer.Average(item => item);
-
-                    string quicktext = String.Empty;
-                    if (speed == 0)
-                    {
-                        quicktext = $"-";
-                    }
-                    else if (speed > 1 && speed < 1024)
-                    {
-                        quicktext = $"{speed.ToString("#")} KB/s";
-                    }
-                    else if (speed > 1024)
-                    {
-                        quicktext = $"{(speed / 100f).ToString("#.#")} MB/s";
-                    }
-
-                    this.Dispatcher.Invoke(() =>
-                    {
-                        this.lbl.Text = speed == 0 ? String.Empty : quicktext;
-                    });
-
-                    buffer.Clear();
-                }
+                    this.lbl.Text = receivedSpeed;
+                    this.lblUp.Text = sentSpeed;
+                });
             }
             catch (Exception ex)
             {
                 eventLog.WriteEntry(ex.ToString(), EventLogEntryType.Error);
-                this._timer.Stop();
             }
-        }
-
-        static long prevValue;
-        static double Speed(long received)
-        {
-            long recievedBytes = received - prevValue;
-            if (recievedBytes > 0)
-            {
-                Debug.WriteLine($"{recievedBytes}={(recievedBytes / 1024f).ToString("#.##")}");
-            }
-            return (recievedBytes / 1024f);
         }
     }
 }
